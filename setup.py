a0ebd0fb55f4bd65742655ae27aafafd03991a50
Jason Ardell <ardell@gmail.com>
2018-11-26 08:06:25 -0700

    Bump version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="tariffizer-classifier-utils",
-    version="0.0.1",
+    version="0.0.2",
     author_email="dev@shiplify.com",
     description="This repo is used to share code between the classifiers.",
     packages=setuptools.find_packages()
